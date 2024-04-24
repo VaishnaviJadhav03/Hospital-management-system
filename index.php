9a1df09865f758197fa4bc7c6484d152f2c43e3a
VaishnaviJadhav03 <154580282+VaishnaviJadhav03@users.noreply.github.com>
2024-04-24 08:19:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,58 @@
+ 
+<!DOCTYPE html> 
+<html lang="en"> 
+
+<head> 
+	<meta charset="UTF-8"> 
+	<meta name="viewport" content= 
+	"width=device-width, initial-scale=1.0"> 
+	<title>Hospital Management System</title> 
+	<style> 
+		body { 
+			background-color: white; 
+			color: #D6ED17FF; 
+			font-family: Arial, sans-serif; 
+			margin: 0; 
+			padding: 0; 
+			text-align: center; 
+		} 
+
+		h1 { 
+			background-color: #D6ED17FF; 
+			color: black !important; 
+			padding: 20px; 
+			margin-bottom: 0; 
+		} 
+
+		
+
+		li { 
+			display: inline-block; 
+			margin: 10px; 
+		} 
+
+		a { 
+			text-decoration: none; 
+			color: #606060FF; 
+			background-color: #D6ED17FF; 
+			padding: 10px 20px; 
+			border-radius: 5px; 
+		} 
+
+		a:hover { 
+			background-color: #606060FF; 
+			color: #D6ED17FF; 
+		} 
+	</style> 
+</head> 
+
+<body> 
+	 
+	<li>
+		<a href="view_patients.php"> view patient Details</a>
+	</li>
+	</body>
+</html>
+</body> 
+
+</html>
